34a42cc8ec261cab587a2a59a6caa455db665e88
Yang Liu <liuyang12@focusmedia.cn>
2019-07-19 13:48:05 +0800

    update

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ class FrameDetectionModel(nn.Module):
     def __init__(self):
         super(FrameDetectionModel, self).__init__()
         resnet = models.resnet50(pretrained=True)
-        # Remove linear and pool layers (since we're not doing classification)
+        # Remove linear layer (since we're not doing classification)
         modules = list(resnet.children())[:-1]
         self.resnet = nn.Sequential(*modules)
         self.fc = nn.Linear(2048, 8)
