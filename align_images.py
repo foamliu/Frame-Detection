fe9ffae7f28d4d298b029917df3f3e2141d3f071
Yang Liu <liuyang12@focusmedia.cn>
2019-11-27 17:57:45 +0800

    update

--- align_images.py
+++ align_images.py
@@ -32,42 +32,46 @@ if __name__ == "__main__":
     for sample in tqdm(samples):
         dir = sample['dir']
         file = sample['file']
+        is_sample = sample['is_sample']
         src_path = '{}{}/{}'.format(src_folder, dir, file)
-
         raw = cv.imread(src_path)
-        h, w = raw.shape[:2]
-        img = cv.resize(raw, (im_size, im_size))
-        img = img[..., ::-1]  # RGB
-        img = transforms.ToPILImage()(img)
-        img = transformer(img)
-        img = img.unsqueeze(0)
-
-        with torch.no_grad():
-            outputs = model(img)
-
-        output = outputs[0].cpu().numpy()
-        output = np.reshape(output, (4, 1, 2))
-
-        for p in output:
-            p[0][0] = p[0][0] * w
-            p[0][1] = p[0][1] * h
-
-        # output = output * im_size
-        # print('output: ' + str(output))
-        # print('output.shape: ' + str(output.shape))
-
-        # img = draw_bboxes(raw, output, size=15)
-        # cv.imwrite('test/result.jpg', img)
-
-        src_pts = output
-        dst_pts = np.float32([[0, 0], [0, im_size], [im_size, im_size], [im_size, 0]]).reshape(-1, 1, 2)
-        M, _ = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-        # print(M)
-
-        img = cv.warpPerspective(raw, M, (im_size, im_size), cv.INTER_CUBIC)
-
         dst_path = os.path.join(dst_folder, dir)
         ensure_folder(dst_path)
         dst_path = os.path.join(dst_path, file)
 
-        cv.imwrite(dst_path, img)
+        if not is_sample:
+            h, w = raw.shape[:2]
+            img = cv.resize(raw, (im_size, im_size))
+            img = img[..., ::-1]  # RGB
+            img = transforms.ToPILImage()(img)
+            img = transformer(img)
+            img = img.unsqueeze(0)
+
+            with torch.no_grad():
+                outputs = model(img)
+
+            output = outputs[0].cpu().numpy()
+            output = np.reshape(output, (4, 1, 2))
+
+            for p in output:
+                p[0][0] = p[0][0] * w
+                p[0][1] = p[0][1] * h
+
+            # output = output * im_size
+            # print('output: ' + str(output))
+            # print('output.shape: ' + str(output.shape))
+
+            # img = draw_bboxes(raw, output, size=15)
+            # cv.imwrite('test/result.jpg', img)
+
+            src_pts = output
+            dst_pts = np.float32([[0, 0], [0, im_size], [im_size, im_size], [im_size, 0]]).reshape(-1, 1, 2)
+            M, _ = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+            # print(M)
+
+            img = cv.warpPerspective(raw, M, (im_size, im_size), cv.INTER_CUBIC)
+            cv.imwrite(dst_path, img)
+
+        else:
+            img = cv.resize(raw, (im_size, im_size), cv.INTER_CUBIC)
+            cv.imwrite(dst_path, img)
