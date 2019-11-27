5e0dd4808ee10af01d0ad4ca5d29fafdaf4db77c
Yang Liu <liuyang12@focusmedia.cn>
2019-11-27 17:34:24 +0800

    update

---
+++ align_images.py
@@ -0,0 +1,18 @@
+import pickle
+import random
+
+import cv2 as cv
+import numpy as np
+import torch
+from torchvision import transforms
+
+from config import im_size, pickle_file, num_train
+from data_gen import data_transforms
+from utils import ensure_folder, draw_bboxes
+
+if __name__ == "__main__":
+    with open(pickle_file, 'rb') as file:
+        data = pickle.load(file)
+
+    samples = [item for item in data if item['pts']]
+    print(samples[0])
